d30f0644dc297b7e9918f66f55b6fe3a97c8c637
Srinivasula Reddy Mallela <Srinivasulareddy.in@gmail.com>
2021-12-31 18:28:51 +0530

    secound commit

---
+++ Gymone/Gymone.API/Common/DependencyInjectionConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gymone.API.Common
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            //services.AddScoped<BookStoreDbContext>();
+
+            //services.AddScoped<ICategoryRepository, CategoryRepository>();
+            //services.AddScoped<IBookRepository, BookRepository>();
+
+            //services.AddScoped<ICategoryService, CategoryService>();
+            //services.AddScoped<IBookService, BookService>();
+
+            return services;
+        }
+    }
+}
---
+++ Gymone/Gymone.API/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gymone.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    // To load dlls.
+                    var p = System.Reflection.Assembly.GetEntryAssembly().Location;
+                    p = p.Substring(0, p.LastIndexOf(@"\") + 1);
+                    webBuilder.UseSetting(WebHostDefaults.SuppressStatusMessagesKey, "True");
+                    webBuilder.UseContentRoot(p);
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
